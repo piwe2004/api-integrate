e7e5e14d983ae184bb44670394b6d63c841e65da
piwe2004 <piwe2004@gmail.com>
2021-05-25 15:52:04 +0900

    API연동 연습

--- src/User.js
+++ src/User.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from "./useAsync";
+import {useAsync} from "react-async";
 import Users from "./Users";
 
 
@@ -11,12 +11,16 @@ async function getUsers(){
 
 
 function User(){
-    const [state, refetch] = useAsync(getUsers, [], true);
-    const [userId, setUserId ] = useState(null)
-    const {loading, data: users, error} = state;
-    if (loading) return <div>로딩중..</div>;
+    const [userId, setUserId ] = useState(null);
+    const{
+        data:users, error, isLoading, reload
+    } = useAsync({
+        promiseFn:getUsers
+    });
+
+    if (isLoading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return <button onClick={refetch}>불러오기</button>;
+    if (!users) return <button onClick={reload}>불러오기</button>;
 
     return (
         <>
@@ -27,7 +31,7 @@ function User(){
                 </li>
             ))}
         </ul>
-        <button onClick={refetch}>다시불러오기</button>
+        <button onClick={reload}>다시불러오기</button>
         {userId && <Users id={userId} />}
         </>
     );
